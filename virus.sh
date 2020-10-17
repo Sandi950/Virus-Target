6780fae9ff5e11039fea332bc62801ec6381beea
Hacker Gaming <69993535+Sandi950@users.noreply.github.com>
2020-10-17 18:43:11 +0700

    Virus

---
+++ virus.sh
@@ -0,0 +1,146 @@
+clear
+cyan='\e[0;36m'
+purple='\e[0;35m'
+brown='\e[0;33m'
+lightgray='\e[0;37m'
+darkgray='\e[1;30m'
+lightblue='\e[1;34m'
+lightgreen='\e[1;32m'
+lightcyan='\e[1;36m'
+lightred='\e[1;31m'
+lightpurple='\e[1;35m'
+yellow='\e[1;33m'
+white='\e[1;37m'
+sleep 1
+figlet VIRUS | lolcat
+sleep 1
+echo "=======================[]=========================" | lolcat
+echo "•              [ Author : Sandi950 ]             •" | lolcat
+echo "•             [ YouTube : Sandi Yt  ]            •" | lolcat
+echo "=======================[]=========================" | lolcat
+echo
+echo
+echo -n "No Target : " | lolcat ;read ju
+echo
+echo "Loading...." | lolcat
+sleep 25
+echo
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo "Mengirim Virus Ke Nomor $ju Sukses" | lolcat
+sleep 1
+echo
+echo
+sleep 3
+figlet SUKSES | lolcat
+exit
